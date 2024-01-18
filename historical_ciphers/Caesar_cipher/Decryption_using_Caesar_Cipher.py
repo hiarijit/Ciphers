1507177b3bcf3a9fc51eb80cb71daa53d56b36fe
Arijit <68233664+hiarijit@users.noreply.github.com>
2024-01-19 00:41:55 +0530

    Update Decryption_using_Caesar_Cipher.py

--- historical_ciphers/Caesar_cipher/Decryption_using_Caesar_Cipher.py
+++ historical_ciphers/Caesar_cipher/Decryption_using_Caesar_Cipher.py
@@ -4,8 +4,11 @@ shift = int(input("Enter the key (shift) to decrypt the ciphertext (0-25): "))
 print()
 plaintext = ""
 for j in range(len(x)):
-    for k in range(len(alphabet)):
-        if alphabet[k] == x[j].lower():
-            plaintext += alphabet[(k-shift) % 26]
-            break
+    if x[j] in alphabet:
+        for k in range(len(alphabet)):
+            if alphabet[k] == x[j].lower():
+                plaintext += alphabet[(k - shift) % 26]
+                break
+    else:
+        plaintext += x[j]
 print("The corresponding ciphertext is : " + plaintext)
