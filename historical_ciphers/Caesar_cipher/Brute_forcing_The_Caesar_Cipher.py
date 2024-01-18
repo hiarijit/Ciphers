7acbe48b64e1131b76d849ee78900672ac26d4f3
Arijit <68233664+hiarijit@users.noreply.github.com>
2024-01-19 00:41:18 +0530

    Update Brute_forcing_The_Caesar_Cipher.py
    Allows the digits and non-alphabet characters used in the input to be reflected as it is in the output.

--- historical_ciphers/Caesar_cipher/Brute_forcing_The_Caesar_Cipher.py
+++ historical_ciphers/Caesar_cipher/Brute_forcing_The_Caesar_Cipher.py
@@ -4,9 +4,12 @@ print()
 for i in range(26):
     string = ""
     for j in range(len(x)):
-        for k in range(len(alphabet)):
-            if alphabet[k] == x[j].lower():
-                string += alphabet[(k+i) % 26]
-                break
+        if x[j] in alphabet:
+            for k in range(len(alphabet)):
+                if alphabet[k] == x[j].lower():
+                    string += alphabet[(k + i) % 26]
+                    break
+        else:
+            string += x[j]
     print(string)
 
